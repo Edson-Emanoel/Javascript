f58ec1d24d017e1835fb558d3153490503a01554
Edson-Emanoel <156195854+Edson-Emanoel@users.noreply.github.com>
2024-08-16 16:04:35 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,12 @@
+const pokemons = [
+    'Rayquaza',
+    'Ampharos',
+    'Greninja',
+    'Zygarde', 
+    'Zoroark',
+    'Mewtwo'
+];
+
+for(let i = 0; i < pokemons.length; i++){
+    console.log(pokemons[i]);
+}
